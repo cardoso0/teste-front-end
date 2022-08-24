845f2a8394c6d6df192006eef67f8cb3a13e459a
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-23 22:14:15 -0300

    style(PD): vitrine responsive for tablet

--- src/js/slides.js
+++ src/js/slides.js
@@ -4,6 +4,16 @@ const slids = async () => {
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        }
+      },
+    ]
   });
 }
 slids()
